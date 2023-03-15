b5f6e9c73b4f00f4fcd4e8c1a7c893df086723b4
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-03-15 10:54:29 -0400

    Kick off the project

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -57,5 +57,10 @@ class JetstreamServiceProvider extends ServiceProvider
             'create',
             'update',
         ])->description('Editor users have the ability to read, create, and update.');
+
+
+        Jetstream::role('reader', 'Reader', [
+            'read',
+        ])->description('Can Read books');
     }
 }
